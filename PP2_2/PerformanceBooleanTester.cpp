b99022d505e99556b855948a138dfe2a67b59c1c
Ksenia <ksenia.kyselgova@gmail.com>
2025-10-26 18:25:47 +0200

    improove

--- PP2_2/PerformanceBooleanTester.cpp
+++ PP2_2/PerformanceBooleanTester.cpp
@@ -3,7 +3,7 @@
 bool PerformanceBooleanTester::Measure(const TAlg& alg)
 {
     const auto clock = std::chrono::high_resolution_clock();
-    auto durations = std::vector<TMcs>(GetCount(), 0);
+    auto avgDuration = TMcs(0);
     auto res = false;
 
     for (int i = 0; i < GetCount(); i++)
@@ -15,15 +15,10 @@ bool PerformanceBooleanTester::Measure(const TAlg& alg)
         const auto end = clock.now();
         const auto duration = end - start;
 
-        durations[i] = static_cast<TMcs>(std::chrono::duration_cast<std::chrono::microseconds>(duration).count());
+        avgDuration += static_cast<TMcs>(std::chrono::duration_cast<std::chrono::microseconds>(duration).count());
     }
 
-    auto avgDuration = TMcs(0);
-    for (auto dur : durations)
-    {
-        avgDuration += dur;
-    }
-    avgDuration /= count;
+    avgDuration /= GetCount();
     measureRes = avgDuration;
 
     return res;
